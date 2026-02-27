d6b73bc1a6b776634d7f9dc29a7deaa643441dad
Grace Rosario <gracerosario398@gmail.com>
2026-02-27 15:17:45 +0530

    feat: close share tooltip when clicking outside footer

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-
 // const tooltip = document.querySelector('.card__tooltip');
 // const userInfo = document.querySelector('.card__user-info');
-const shareBtn = document.querySelector('.share-button');
-const card = document.querySelector('.card');
+const shareBtn = document.querySelector(".share-button");
+const card = document.querySelector(".card");
+const cardFooter = document.querySelector(".card__footer");
 
-shareBtn.addEventListener('click', (e) => {
-    card.classList.toggle('card--share-active')
+shareBtn.addEventListener("click", (e) => {
+  card.classList.toggle("card--share-active");
+});
+
+document.addEventListener("click", (e) => {
+  if (!cardFooter.contains(e.target)) {
+    card.classList.remove("card--share-active");
+  }
 });
